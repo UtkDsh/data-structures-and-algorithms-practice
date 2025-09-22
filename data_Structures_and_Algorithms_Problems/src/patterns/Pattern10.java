fb03cbd9bdd4fe8f64b0bf1c22ec33b80b75b907
UtkDsh <deshmukhutkarsh6@gmail.com>
2025-09-22 20:08:25 +0530

    Solved Pattern Problem no.10

--- data_Structures_and_Algorithms_Problems/src/patterns/Pattern10.java
+++ data_Structures_and_Algorithms_Problems/src/patterns/Pattern10.java
@@ -1,42 +1,24 @@
-/*
- Pattern
- 
- * 
- *  * 
- *  *  * 
- *  *  *  * 
- *  *  *  *  * 
- 
-
- 
- */
-
 package patterns;
 
 import java.util.Scanner;
 
-public class Pattern2 {
+public class Pattern10 {
 	
 	public static void printPattern(int n)
 	{
-		for(int i=1;i<=n;i++)
-		{
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-				
+		Pattern2 p1=new Pattern2();
+		Pattern5 p2=new Pattern5();
+		p1.printPattern(n);
+		p2.printPattern(n);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter upto which you want to print Pattern");
 		int n=sc.nextInt();
 		printPattern(n);
+		
 
 	}
 
